78a303716731309353abb14d7c7a23892bdae797
AlexandrLegchilov <Alexandr_Legchilov@epam.com>
2023-09-05 11:40:33 +0300

    attach ER and AR in case of image size mismatch

--- src/visual.ts
+++ src/visual.ts
@@ -3,6 +3,9 @@ import memory from '@qavajs/memory';
 import { PNG } from 'pngjs';
 import pixelmatch from 'pixelmatch';
 
+const BASE64 = 'base64';
+const BASE64_IMAGE = 'base64:image/png';
+
 async function compare(
     world: IWorld,
     actual: string,
@@ -11,24 +14,31 @@ async function compare(
 ) {
     const actualValue = await memory.getValue(actual);
     const expectedValue = await memory.getValue(expected);
-    const actualBuffer = typeof actualValue !== "string" ? actualValue : Buffer.from(actualValue, 'base64');
-    const expectedBuffer = typeof expectedValue !== "string" ? expectedValue : Buffer.from(expectedValue, 'base64');
+    const actualBuffer = typeof actualValue !== "string" ? actualValue : Buffer.from(actualValue, BASE64);
+    const expectedBuffer = typeof expectedValue !== "string" ? expectedValue : Buffer.from(expectedValue, BASE64);
     const actualScreenshotImage = PNG.sync.read(actualBuffer);
     const expectedScreenshotImage = PNG.sync.read(expectedBuffer);
     const { width, height } = expectedScreenshotImage;
-    const diff = new PNG({width, height});
-    const delta = pixelmatch(
-        actualScreenshotImage.data,
-        expectedScreenshotImage.data,
-        diff.data,
-        width,
-        height,
-        { threshold }
-    );
+    const diff = new PNG({ width, height});
+    let delta;
+    try {
+        delta = pixelmatch(
+            actualScreenshotImage.data,
+            expectedScreenshotImage.data,
+            diff.data,
+            width,
+            height,
+            { threshold }
+        );
+    } catch (e) {
+        world.attach(actualBuffer.toString(BASE64), BASE64_IMAGE);
+        world.attach(expectedBuffer.toString(BASE64), BASE64_IMAGE);
+        throw e;
+    }
     if (delta > 0) {
-        world.attach(actualBuffer.toString('base64'), 'base64:image/png');
-        world.attach(expectedBuffer.toString('base64'), 'base64:image/png');
-        world.attach(PNG.sync.write(diff).toString('base64'), 'base64:image/png');
+        world.attach(actualBuffer.toString(BASE64), BASE64_IMAGE);
+        world.attach(expectedBuffer.toString(BASE64), BASE64_IMAGE);
+        world.attach(PNG.sync.write(diff).toString(BASE64), BASE64_IMAGE);
         throw new Error('Images are not equal');
     }
 }
